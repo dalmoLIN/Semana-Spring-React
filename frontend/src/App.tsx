5451b85e92d5fa129f5a7ba4284227e8c573df19
Luiz Felipe Dalmolin <luizfelipedalmolin@hotmail.com>
2022-07-15 00:47:26 -0300

    First Component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,13 @@
+import NotificationButton from "./components"
 
 function App() {
- return (<h1>Ola Mundo</h1>)
+  return (
+    <>
+      <h1>Ola Mundo</h1>
+      <NotificationButton />
+      <NotificationButton />
+    </>
+  )
 }
 
 export default App
